479871cf6b10f3bf673a67d19c1515c7b9b1aa2e
Jeureka <Jeureka@git.com>
2022-02-26 22:34:49 +0800

    添加 RestfulAccessDeniedHandler类 功能为当访问接口没有权限时，自定义的返回结果

---
+++ src/main/java/pers/jl/jmall/component/RestfulAccessDeniedHandler.java
@@ -0,0 +1,33 @@
+package pers.jl.jmall.component;
+
+import cn.hutool.json.JSONUtil;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import pers.jl.jmall.common.api.CommonResult;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @ClassName: RestfulAccessDeniedHandler.java
+ * @Description: 当访问接口没有权限时，自定义的返回结果
+ * @author: JL Du
+ * @date: 2022/2/26 22:33
+ * @version: 1.0.0
+ */
+
+@Component
+public class RestfulAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException e) throws IOException, ServletException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        response.getWriter().println(JSONUtil.parse(CommonResult.forbidden(e.getMessage())));
+        response.getWriter().flush();
+    }
+}
