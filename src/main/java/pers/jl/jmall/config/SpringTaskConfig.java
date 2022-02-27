f2de7f568c7bcc5d2eb37473d3359f9c9eac8ac2
Jeureka <Jeureka@git.com>
2022-02-27 15:50:50 +0800

    添加SpringTask配置类

---
+++ src/main/java/pers/jl/jmall/config/SpringTaskConfig.java
@@ -0,0 +1,16 @@
+package pers.jl.jmall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * SpringTask配置类
+ * @author: JL Du
+ * @date: 2022/2/27 15:44
+ * @version: 1.0.0
+ */
+
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
