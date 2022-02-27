4dd53c9e0bb65922b884a1a9e9f9282c52925b98
Jeureka <Jeureka@git.com>
2022-02-27 16:09:46 +0800

    Jmall整合SpringTask实现定时任务

---
+++ src/main/java/pers/jl/jmall/component/OrderTimeOutCancelTask.java
@@ -0,0 +1,27 @@
+package pers.jl.jmall.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 订单超时取消并解锁库存的定时器
+ * @author: JL Du
+ * @date: 2022/2/27 15:53
+ * @version: 1.0.0
+ */
+@Component
+public class OrderTimeOutCancelTask {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderTimeOutCancelTask.class);
+
+    /**
+     * cron表达式：Seconds Minutes Hours Day-ofMonth Month Day-ofWeek [Year]
+     * 每10分钟扫描一次，扫描设定超时时间之前下的订单，如果没支付则取消该订单
+     */
+    @Scheduled(cron = "0 0/10 * ? * ?")
+    private void cancelTimeOutOrder() {
+        // TODO: 此处应调用取消订单的方法，具体查看mall项目源码
+        LOGGER.info("取消订单，并根据sku编号释放锁定库存");
+    }
+}
